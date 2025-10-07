71ff195f0857f2ec75e3ef26310dfffbe16cc9d5
Edwin Campos <ecamposc97@gmail.com>
2025-10-07 14:20:22 -0500

    Correction data sent

--- send-data/send.go
+++ send-data/send.go
@@ -33,11 +33,12 @@ func main() {
 
 	consumption := 0
 	for {
-		timeStr := time.Now().Format("2006-01-02 15:04")
+		dateStr := time.Now().Format("0201'06")
+		hourStr := time.Now().Format("150400.000000000")
 		consumption += 1000
-		data := fmt.Sprintf("tiempo:3:%s;WHrecibidos:1:%d;", timeStr, consumption)
-		message := fmt.Sprintf("NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA; NA;;NA;%s", data)
-		CRC = ""
+		data := fmt.Sprintf("WHrecibidos:1:%d;", consumption)
+		message := fmt.Sprintf("%s;%s;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;;NA;%s;", dateStr, hourStr, data)
+		CRC = crcChecksum([]byte(message))
 		res, err = WritePacket(fmt.Sprintf("#D#%s%s\r\n", message, CRC), conn)
 		if err != nil {
 			log.Println("Writing error", err)
