a998ad0f3aadf4db3e0dfa677bce9ad82abb9ed8
Edmar <ecamposc97@gmail.com>
2025-10-13 15:25:59 -0500

    Updates to deployment!

--- host-env/start-service.sh
+++ host-env/start-service.sh
@@ -6,13 +6,13 @@ cd "$APP_DIR" || exit
 
 cd /home/panel/panel/filter-power || exit
 go build -o "$APP_DIR/filter-power" .
-cd "APP_DIR" || exit
+cd "$APP_DIR" || exit
 
 sudo chmod +x get-device-info.sh upload-power.sh
 mkdir -p csv-input csv-save
 
 # Systemd services
-cp save-energy.service save-energy.timer .config/systemd/user/
-systemctl --user daemon-reload
-systemctl enable --user save-energy.timer
-systemctl start --user save-energy.timer
+#cp save-energy.service save-energy.timer .config/systemd/user/
+#systemctl --user daemon-reload
+#systemctl enable --user save-energy.timer
+#systemctl start --user save-energy.timer
