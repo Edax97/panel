3d3508edab8e46ec126e507ab7628ebb8e38e859
Edwin Campos <ecamposc97@gmail.com>
2025-10-14 11:28:00 -0500

    Fixed

--- filter-power/wailonServer/wailonServer.go
+++ filter-power/wailonServer/wailonServer.go
@@ -36,7 +36,7 @@ func (s *WailonServer) SendTimeValue(imei string, date time.Time, value int) (bo
 	log.Println("* Login: ", res)
 
 	hourStr := date.Format("2006.01.02.15.04")
-	data := fmt.Sprintf("time:3:%s;WHrecibidos:1:%d;", hourStr, value)
+	data := fmt.Sprintf("time:3:%s;wh:1:%d;", hourStr, value)
 	message := fmt.Sprintf("NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;;NA;%s", data)
 	CRC = crcChecksum([]byte(message))
 	log.Println("* Data: ", message)
