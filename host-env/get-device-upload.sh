ee8195e0c63a9880fc0ec5fca0995f76245321ab
Edwin Campos <ecamposc97@gmail.com>
2025-10-22 15:57:44 -0500

    Fixes to wailon send

--- host-env/get-device-upload.sh
+++ host-env/get-device-upload.sh
@@ -7,7 +7,7 @@ export CSV_UPLOAD_PATH="/home/panel/csv-save"
 mkdir -p "$CSV_INPUT_PATH" "$CSV_UPLOAD_PATH"
 
 ./get-device-info.sh "$CSV_INPUT_PATH" || {
-  echo "Download device information failed"; exit 1
+  echo "Download device information failed"
 }
 ./upload-power.sh "$CSV_INPUT_PATH" "$CSV_UPLOAD_PATH" || {
   echo "Filter and upload power data failed"; exit 1
