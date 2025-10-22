140455dd68bb52de2cad6b9c4e65faf4f3ddaa13
Edwin Campos <ecamposc97@gmail.com>
2025-10-22 14:09:41 -0500

    Fixess

--- send-data/send.go
+++ send-data/send.go
@@ -33,10 +33,15 @@ func main() {
 
 	consumption := 0
 	for {
-		hourStr := time.Now().Format("2006.01.02.15.04")
-		consumption += 1000
-		data := fmt.Sprintf("time:3:%s;WHrecibidos:1:%d;", hourStr, consumption)
-		message := fmt.Sprintf("NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;;NA;%s", data)
+		t := time.Now()
+		date := t.Format("020106")
+		second := t.Format("150405")
+		data := fmt.Sprintf("time:3:%s/%s,wh:1:%d;", date, second, consumption)
+		message := fmt.Sprintf("%s;%s.000000000;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;;NA;%s", date, second, data)
+		//hourStr := time.Now().Format("2006.01.02.15.04")
+		//consumption += 1000
+		//data := fmt.Sprintf("time:3:%s;WHrecibidos:1:%d;", hourStr, consumption)
+		//message := fmt.Sprintf("NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;NA;;NA;%s", data)
 		CRC = crcChecksum([]byte(message))
 		res, err = WritePacket(fmt.Sprintf("#D#%s%s\r\n", message, CRC), conn)
 		if err != nil {
